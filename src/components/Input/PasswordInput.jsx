e2cac84ac3c0e91e5bf6cc29f9975f9ee2008374
unknown <zxckendrick123@gmail.com>
2024-07-09 15:01:10 +0800

    edited pass input

--- src/components/Input/PasswordInput.jsx
+++ src/components/Input/PasswordInput.jsx
@@ -10,13 +10,13 @@ const PasswordInput = ({value, onChange, placeholder}) => {
     };
 
   return (
-    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
+    <div className='flex items-center bg-transparent border-[1.5px] pr-5 rounded mb-3'>
         <input
             value={value}
             onChange={onChange}
             type={isShowPassword ? "text" : "password"} 
             placeholder={placeholder || "Password"}
-            className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none'
+            className='w-full text-sm bg-transparent p-3 pl-5 mr-3 rounded outline-none'
         />
 
         {isShowPassword ? <FaRegEye
